fdd92729b32497e5bd3edb47eec042535980ef7f
HroBiT <78382357+HroBiT@users.noreply.github.com>
2025-02-10 21:21:26 +0100

    main

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  output: 'export',
+  basePath: '/Pages',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
