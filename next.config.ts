d65e0eba22326fbbaf7985a7259b4cda11c1de35
HroBiT <78382357+HroBiT@users.noreply.github.com>
2025-02-10 16:54:16 +0100

    eh

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone', // Ensure server-side rendering
+  output: 'standalone',
 };
 
 export default nextConfig;
--- src/app/Pages/Posts/page.tsx
+++ src/app/Pages/Posts/page.tsx
@@ -1,23 +1,31 @@
 import { prisma } from "@/lib/db";
-import Link from "next/link";
 
-export default async function PostsPage() {
-  const posts = await prisma.product.findMany();
+interface PostPageProps {
+  params: { id: string };
+}
+
+export default async function PostPage({ params }: PostPageProps) {
+  const postId = parseInt(params.id, 10);
+  const post = await prisma.product.findUnique({
+    where: { id: postId },
+  });
 
   return (
-    <div className="max-w-4xl mx-auto p-6">
-      <h1 className="text-4xl font-bold mb-6 text-center">Wszystkie posty</h1>
-      <ul className="space-y-4">
-        {posts.map((post) => {
-          return (
-            <li key={post.id} className="bg-white shadow-md rounded-lg p-4 hover:bg-gray-100 transition">
-              <Link href={`/Pages/Posts/${post.id}`}  className="text-2xl font-semibold text-blue-600 hover:underline">
-                  {post.name}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+    <div className="bg-white rounded-2xl max-w-2xl mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">Post {post?.name}</h1>
+      <p className="text-lg text-gray-700">{post?.description}</p>      
+      <p>{post?.price} zł</p>
     </div>
   );
+}
+
+// ✅ Dodaj tę funkcję, jeśli używasz output: "export"
+export async function generateStaticParams() {
+  const posts = await prisma.product.findMany({
+    select: { id: true },
+  });
+
+  return posts.map((post) => ({
+    id: post.id.toString(),
+  }));
 }
