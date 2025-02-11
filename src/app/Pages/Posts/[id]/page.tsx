38e0b309dba0cb77d9d68bdbcbe3090a573bf8de
HroBiT <78382357+HroBiT@users.noreply.github.com>
2025-02-11 10:17:06 +0100

    Update page.tsx

--- src/app/Pages/Posts/[id]/page.tsx
+++ src/app/Pages/Posts/[id]/page.tsx
@@ -6,9 +6,6 @@ interface PostPageProps {
   }>;
 }
 
-export function generateStaticParams() {
-  return [{ id: '1' }, { id: '2' }, { id: '3' }]
-}
 
 
 export default async function PostPage({ params }: PostPageProps) {
