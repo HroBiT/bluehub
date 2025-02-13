123c95d29d5e2059c418a10b5f8f57b2c9e9cb57
HroBiT <78382357+HroBiT@users.noreply.github.com>
2025-02-13 15:30:27 +0100

    Resolve merge conflicts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,27 +4,9 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-<<<<<<< HEAD
+
 const prisma = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
 export default prisma;
-=======
-let prisma: PrismaClient;
-
-if (typeof window === "undefined") {
-  if (process.env.NODE_ENV === "production") {
-    prisma = new PrismaClient();
-  } else {
-    if (!globalForPrisma.prisma) {
-      globalForPrisma.prisma = new PrismaClient();
-    }
-    prisma = globalForPrisma.prisma;
-  }
-} else {
-  throw new Error("PrismaClient is unable to run in the browser.");
-}
-
-export { prisma };
->>>>>>> 65898da838e4c6c9417c1e0aef32b6ab1833c9d2
