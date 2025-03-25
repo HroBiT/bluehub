1b72011208cd10ee53e69d456f03e571b15ce7fe
HroBiT <gruca.milosz@gmail.com>
2025-03-25 18:57:13 +0100

    adfsfasd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,21 +7,13 @@ interface Product {
   price: number;
 }
 
-// Funkcja do pobierania danych (serwerowa)
-async function fetchProducts(): Promise<Product[]> {
-  // Przykładowe dane produktów (zastąp je wywołaniem zewnętrznego API, jeśli to konieczne)
-  const products: Product[] = [
-    { id: 1, name: "Product 1", description: "Description 1", price: 100 },
-    { id: 2, name: "Product 2", description: "Description 2", price: 200 },
-    { id: 3, name: "Product 3", description: "Description 3", price: 300 },
-  ];
+const products: Product[] = [
+  { id: 1, name: "Product 1", description: "Description 1", price: 100 },
+  { id: 2, name: "Product 2", description: "Description 2", price: 200 },
+  { id: 3, name: "Product 3", description: "Description 3", price: 300 },
+];
 
-  return products;
-}
-
-export default async function Home() {
-  const products = await fetchProducts();
-
+export default function Home() {
   return (
     <div className="flex justify-center items-center min-h-screen p-4 ">
       <div className="p-6 rounded-lg w-full max-w-6xl">
